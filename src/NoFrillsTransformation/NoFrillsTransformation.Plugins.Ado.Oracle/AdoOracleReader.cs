4c553599f704ba350e80bbac737e132bfe9ae205
Martin Danielsson <martin@danielsson.de>
2015-07-09 10:41:39 +0200

    Added Oracle support (okay, via OracleConnection, which is depreciated, but it still works).

---
+++ src/NoFrillsTransformation/NoFrillsTransformation.Plugins.Ado.Oracle/AdoOracleReader.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OracleClient;
+using System.Linq;
+using System.Text;
+using NoFrillsTransformation.Interfaces;
+
+namespace NoFrillsTransformation.Plugins.Ado.Oracle
+{
+    internal class AdoOracleReader : AdoReader
+    {
+        public AdoOracleReader(IContext context, string config, string sqlQuery)
+            : base(context, config, sqlQuery)
+        {
+        }
+
+        private OracleConnection _sqlConnection;
+        private OracleCommand _sqlCommand;
+        private IDataReader _sqlReader;
+
+        protected override void Initialize()
+        {
+            Context.Logger.Info("AdoOracleReader initializing...");
+            _sqlConnection = new OracleConnection(Config);
+            _sqlCommand = new OracleCommand(SqlQuery, _sqlConnection);
+            _sqlConnection.Open();
+            _sqlReader = _sqlCommand.ExecuteReader();
+            Context.Logger.Info("AdoOracleReader initialized.");
+        }
+
+        protected override IDataReader SqlReader
+        {
+            get
+            {
+                return _sqlReader;
+            }
+        }
+
+        #region IDisposable
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                // free managed resources
+                if (null != _sqlReader)
+                {
+                    _sqlReader.Dispose();
+                    _sqlReader = null;
+                }
+                if (null != _sqlCommand)
+                {
+                    _sqlCommand.Dispose();
+                    _sqlCommand = null;
+                }
+                if (null != _sqlConnection)
+                {
+                    _sqlConnection.Close();
+                    _sqlConnection.Dispose();
+                    _sqlConnection = null;
+                }
+            }
+        }
+        #endregion   
+    }
+}
---
+++ src/NoFrillsTransformation/NoFrillsTransformation.Plugins.Ado.Oracle/AdoOracleReaderFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using NoFrillsTransformation.Interfaces;
+
+namespace NoFrillsTransformation.Plugins.Ado.Oracle
+{
+    [Export(typeof(NoFrillsTransformation.Interfaces.ISourceReaderFactory))]
+    public class AdoOracleReaderFactory : ISourceReaderFactory
+    {
+        private const string PREFIX = "oracle://";
+
+        public bool CanReadSource(string source)
+        {
+            source = source.ToLowerInvariant();
+            return source.StartsWith(PREFIX);
+        }
+
+        public ISourceReader CreateReader(IContext context, string source, string config)
+        {
+            string sqlQuery = source.Substring(PREFIX.Length); // strip PREFIX
+            return new AdoOracleReader(context, config, sqlQuery);
+        }
+
+        public bool SupportsQuery
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
